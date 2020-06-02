d1a77fe6c984310211c9053301a4b18810506a70
guyschwartzberg-student <schwartzberg@mail.tau.ac.il>
2020-06-02 14:04:38 +0300

    changed formatting of the file

--- client/src/components/RecipeList/components/RecipeCard.jsx
+++ client/src/components/RecipeList/components/RecipeCard.jsx
@@ -1,48 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Modal, Button, Tag, Descriptions, Divider } from 'antd';
-import { InfoCircleOutlined, StarOutlined, ExportOutlined, CloseCircleOutlined, StarFilled } from '@ant-design/icons';
+import {
+  Card, Modal, Button, Tag, Descriptions, Divider,
+} from 'antd';
+import {
+  InfoCircleOutlined, StarOutlined, ExportOutlined, CloseCircleOutlined, StarFilled,
+} from '@ant-design/icons';
 import RecipeNames from './RecipeConsts';
 
 const { Meta } = Card;
 
 class RecipeCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showModal: false, viewMore: false };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {showModal: false, viewMore: false,};
-    }
 
-    showModal = () => {
-        this.setState({
-            showModal: true,
-        });
-    };
+  showModal() {
+    this.setState({
+      showModal: true,
+    });
+  }
 
-    hideModal = () => {
-        this.setState({
-            showModal: false,
-        });
-    };
+  hideModal() {
+    this.setState({
+      showModal: false,
+    });
+  }
 
 
   render() {
     const { recipe } = this.props;
-    const { title, link, image: imageURL, total_time, rating, number_of_raters, tags, id } = recipe;
+    const {
+      title, link, image: imageURL, totalTime, rating, numberOfRaters, tags, id,
+    } = recipe;
     const viewMoreText = `${tags.length - 5} more`;
     const { showModal, viewMore } = this.state;
 
     const openRecipe = () => {
-          window.open(link, 'noopener noreferrer');
-      };
+      window.open(link, 'noopener noreferrer');
+    };
 
-    const findSite = (id) => {
-        for (let [siteRegex, siteName] of Object.entries(RecipeNames)) {
-            let site = new RegExp(siteRegex);
-            if (site instanceof RegExp && site.test(id)){
-                return siteName;
-            }
+    const findSite = () => {
+      for (const [siteRegex, siteName] of Object.entries(RecipeNames)) {
+        const site = new RegExp(siteRegex);
+        if (site instanceof RegExp && site.test(id)) {
+          return siteName;
         }
+      }
+      return null;
     };
 
     return (
@@ -62,7 +71,7 @@ class RecipeCard extends React.Component {
 
           actions={[
             <li className="recipe-button" onClick={this.showModal}><InfoCircleOutlined /></li>,
-            <li className="recipe-button" /*onClick={}*/ ><StarOutlined /></li>,
+            <li className="recipe-button"><StarOutlined /></li>,
             <li className="recipe-button" onClick={openRecipe}><ExportOutlined /></li>,
           ]}
         >
@@ -76,50 +85,93 @@ class RecipeCard extends React.Component {
           visible={showModal}
           onCancel={this.hideModal}
           footer={[
-              <Button key="Save">
-                  <StarOutlined />
-              </Button>,
+            <Button key="Save">
+              <StarOutlined />
+            </Button>,
             <Button key="Go" onClick={openRecipe}>
               <ExportOutlined />
             </Button>,
           ]}
         >
-            <img
-                className="recipe-modal-image"
-                src={imageURL}
-                alt={title}
-            />
+          <img
+            className="recipe-modal-image"
+            src={imageURL}
+            alt={title}
+          />
 
-            <Divider/>
-            <Descriptions title="Recipe Info" layout="vertical" colon={false}>
-                <Descriptions.Item label="Website">{ findSite(id) }</Descriptions.Item>
-                <Descriptions.Item label="Cooking Time">{total_time ? total_time + ' minutes' : <span> <CloseCircleOutlined /> Unavailable </span> }</Descriptions.Item>
+          <Divider />
+          <Descriptions title="Recipe Info" layout="vertical" colon={false}>
+            <Descriptions.Item label="Website">{ findSite(id) }</Descriptions.Item>
+            <Descriptions.Item label="Cooking Time">
+              {totalTime ? `${totalTime} minutes` : (
+                <span>
+                  {' '}
+                  <CloseCircleOutlined />
+                  {' '}
+                  Unavailable
+                  {' '}
+                </span>
+              ) }
+            </Descriptions.Item>
 
-                <Descriptions.Item label="Rating">{ rating ? <span>  {parseFloat((rating * 5).toFixed(2))}/5 <StarFilled /> from { number_of_raters } {number_of_raters > 1 ? 'raters' : 'rater'} </span> : <span> <CloseCircleOutlined /> Unavailable </span> }</Descriptions.Item>
+            <Descriptions.Item label="Rating">
+              { rating ? (
+                <span>
+                  {' '}
+                  {parseFloat((rating * 5).toFixed(2))}
+                  /5
+                  {' '}
+                  <StarFilled />
+                  {' '}
+                  from
+                  {' '}
+                  { numberOfRaters }
+                  {' '}
+                  {numberOfRaters > 1 ? 'raters' : 'rater'}
+                  {' '}
+                </span>
+              ) : (
+                <span>
+                  {' '}
+                  <CloseCircleOutlined />
+                  {' '}
+                  Unavailable
+                  {' '}
+                </span>
+              ) }
+            </Descriptions.Item>
 
             <Descriptions.Item label="Tags" span={3}>
-                {
+              {
                     tags.map((tag, index) => (
-                        <Tag
-                        className='recipe-tag'
-                        visible={index < 5 || viewMore}> <span> {tag } </span></Tag>
+                      <Tag
+                        className="recipe-tag"
+                        visible={index < 5 || viewMore}
+                      >
+                        {' '}
+                        <span>
+                          {' '}
+                          {tag }
+                          {' '}
+                        </span>
+                      </Tag>
                     ))
                 }
-                {tags.length > 5 && (
-                    <Button
-                        className="more-tags-button"
-                        onClick={() => {
-                            this.setState({ viewMore: !viewMore });
-                        }}
-                        size="small"
-                    >
-                        <span>
-                            {!viewMore ? viewMoreText : 'Show less'}
-                        </span>
-                    </Button>
-                )}
+              {tags.length > 5 && (
+                <Button
+                  className="more-tags-button"
+                  onClick={() => {
+                    this.setState({ viewMore: !viewMore });
+                  }}
+                  size="small"
+                >
+                  <span>
+                    {!viewMore ? viewMoreText : 'Show less'}
+                  </span>
+                </Button>
+              )}
             </Descriptions.Item>
-            </Descriptions>
+          </Descriptions>
         </Modal>
       </>
     );
@@ -131,6 +183,11 @@ RecipeCard.propTypes = {
     title: PropTypes.string,
     link: PropTypes.string,
     image: PropTypes.string,
+    totalTime: PropTypes.number,
+    rating: PropTypes.number,
+    numberOfRaters: PropTypes.number,
+    tags: PropTypes.array,
+    id: PropTypes.string,
   }).isRequired,
 };
 
