c049003568e6185b26be2188664b493c263fe38b
Hagai Shapira <hagais@gmail.com>
2020-07-07 13:27:53 +0300

    eslint fixups

--- api/users/users.js
+++ api/users/users.js
@@ -73,9 +73,7 @@ router.post('/preferences', asyncHandler(async (request, response, next) => {
 router.get('/get_recipes', asyncHandler(async (request, response, next) => {
   await User.findById(request.user.sub, 'recipes').exec()
     .catch((err) => next(err))
-    .then((user) => {
-      return search.searchIdFunc(user.recipes);
-    })
+    .then((user) => search.searchIdFunc(user.recipes))
     .then((query) => {
       response.send({
         // eslint-disable-next-line no-underscore-dangle
