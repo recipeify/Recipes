f95be20a905c78ece101741c22db554f33cf9ec3
Hagai Shapira <hagais@gmail.com>
2020-07-07 16:19:49 +0300

    with cloudinary and proper alignment

--- api/docs/arch_diagram.py
+++ api/docs/arch_diagram.py
@@ -7,37 +7,53 @@ from diagrams.onprem.vcs import Github
 from diagrams.saas.identity import Auth0
 from diagrams.saas.cdn import Cloudflare
 from diagrams.saas.recommendation import Recombee
+from diagrams.saas.media import Cloudinary
 
 
-with Diagram("Recipeify Architecture"):
+def main():
+    with Diagram("Recipeify Architecture", direction='LR'):
 
-    with Cluster("Github"):
-        ci = Github("Actions CI/CD")
-        Github("Open Source Repo") >> ci
+        with Cluster("Github"):
+            ci = Github("Actions CI/CD")
+            Github("Open Source Repo") >> ci
 
 
 
-    with Cluster("Cloudfare"):
-        clf = Cloudflare("CDN\n DDoS Protection")
+        with Cluster("Cloudfare"):
+            clf = Cloudflare("CDN\n DDoS Protection")
 
-    with Cluster("AWS"):
-        with Cluster("VPC"):
-            elb = ELB("Elastic Load Balancer")
-            eb = EB("Elastic Beanstalk")
-            es = ES("Elasticsearch")
-            lmda = Lambda("Lambda")
-        ecr = ECR("Container Registry")
+        with Cluster("AWS", direction='LR'):
+            with Cluster("VPC"):
+                elb = ELB("Elastic Load Balancer")
+                eb = EB("Elastic Beanstalk")
+                es = ES("Elasticsearch")
+                lmda = Lambda("Lambda")
+            ecr = ECR("Container Registry")
 
-    with Cluster("3rd Party SaaS", direction='LR', graph_attr={'newrank': 'true', 'rank': 'same'}):
-        with Cluster("MongoDB Atlas"):
-            eb >> MongoDB("Users DB")
+        with Cluster("3rd Party SaaS", direction='LR'):
+            with Cluster("MongoDB Atlas"):
+                mongo = MongoDB("Users DB")
 
-        with Cluster("Recombee"):
-            eb >> Recombee("Recommendation\n Engine")
+            with Cluster("Recombee"):
+                rec = Recombee("Recommendation\n Engine")
 
-        with Cluster("Auth0"):
-            eb >> Auth0("Oauth2 Integrator")
+            with Cluster("Auth0"):
+                auth = Auth0("Oauth2 Integrator")
 
-    eb << ecr << ci
-    es << lmda
-    clf >> elb >> eb >> es
+            with Cluster("Cloudinary"):
+                cl = Cloudinary("Image Caching\n and Proccessing")
+
+            auth >> Edge(style="invis") >> mongo >> Edge(style="invis") >> cl >> Edge(style="invis") >> rec
+
+        eb >> auth
+        eb >> mongo
+        eb >> cl
+        eb >> rec
+
+        eb << ecr << ci
+        es << lmda
+        clf >> elb >> eb >> es
+
+
+if __name__ == "__main__":
+    main()
