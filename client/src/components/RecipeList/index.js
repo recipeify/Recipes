54cc8e052346d23f81d9f4a24c28654b02c7f3a7
Noa Leshem <nleshem@redhat.com>
2020-07-07 18:04:44 +0300

    Ahhh yes... explore client now work :

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import RecipeList from './RecipeList';
-import { scrollRecipes, fetchRecipesByFilters } from '../../actions/recipeActions';
+import { scrollRecipes } from '../../actions/recipeActions';
 
 const mapStateToProps = (state, ownProps) => {
   const { siteMode } = ownProps;
@@ -25,33 +25,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  getRecipesByFilters: (
-    freeText,
-    include,
-    exclude,
-    diet,
-    dishType,
-    cuisine,
-    toCookTime,
-    fromCookTime,
-    from,
-    size,
-    token,
-  ) => dispatch(
-    fetchRecipesByFilters(
-      freeText,
-      include,
-      exclude,
-      diet,
-      dishType,
-      cuisine,
-      toCookTime,
-      fromCookTime,
-      from,
-      size,
-      token,
-    ),
-  ),
   scrollRecipes: (
     freeText,
     include,
