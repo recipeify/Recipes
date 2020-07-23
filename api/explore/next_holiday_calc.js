df6cece0a5ccb8b6e512692a86b665a10e0f9baf
yuval <yuvalbardugo95@gmail.com>
2020-07-23 19:13:33 +0300

    Added Tu B'Av as a holiday, made sure we only search for food related jewish holidays.

--- api/explore/next_holiday_calc.js
+++ api/explore/next_holiday_calc.js
@@ -1,6 +1,18 @@
 const Hebcal = require('hebcal');
 const moment = require('moment-holiday');
 
+const hebHolidays = ['Chanukah',
+  'Sukkot',
+  'Pesach',
+  'Rosh Hashana',
+  "Tu B'Shvat",
+  'Purim',
+  "Lag B'Omer",
+  'Shavuot',
+  "Yom HaAtzma'ut",
+  "Tu B'Av",
+];
+
 function nextHoliday(tz) {
   /* tz = time zone offset recieved from the client side */
   /* getting the server time time zone offset */
@@ -10,13 +22,24 @@ function nextHoliday(tz) {
   if (tz.search('Israel') >= 0) {
     /* assuming Israeli users are jewish.
         This is obviously false but we'll use it for now */
+    const year = new Hebcal();
     let hday = new Hebcal.HDate();
+    const tuBAv = new Hebcal.HDate(15, 'Av');
+    year.addHoliday(new Hebcal.holidays.Event(tuBAv, "Tu B'Av", Hebcal.holidays.masks.USER_EVENT));
+    console.log(tuBAv.holidays());
     hday.il = true;
     hday.setCity('Jerusalem');
 
     for (let i = 0; i < 21; i += 1) {
       if (hday.holidays().length > 0) {
-        return hday.holidays()[0].split(':')[0].desc[0].replace(/[0-9]/g, '');
+        const holiday = hday.holidays()[0].desc[0].replace(/[0-9]/g, '').split(':')[0];
+        console.log(holiday);
+        if (hebHolidays.includes(holiday)) {
+          return holiday;
+        }
+        if (['Shmini Atzeret', 'Simchat Torah'].includes(holiday)) {
+          return 'Sukkot';
+        }
       }
       hday = hday.next();
     }
